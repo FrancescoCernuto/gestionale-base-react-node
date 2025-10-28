a9910f7c3f355d6cc69ef8a7953be3265a883f69
Francesco.Cernuto <cernutofrancesco2@gmail.com>
2025-10-28 17:33:23 +0100

    fix hook

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,23 +1,28 @@
 /**
- * main.jsx
- * Punto d'ingresso frontend + Toaster per notifiche
+ * main.jsx - Monta correttamente Router + StoreProvider + Toaster
  */
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/main.css";
+import { StoreProvider } from "./context/StoreContext";
 import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-    <Toaster
-      position="bottom-right"
-      toastOptions={{
-        duration: 5000,
-        style: { background: "#343a40", color: "#fff" },
-      }}
-    />
+    <BrowserRouter>
+      <StoreProvider>
+        <App />
+        <Toaster
+          position="bottom-right"
+          toastOptions={{
+            duration: 5000,
+            style: { background: "#343a40", color: "#fff" },
+          }}
+        />
+      </StoreProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
